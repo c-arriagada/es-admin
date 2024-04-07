d3cf2efb61b7fd3f8cb681c3da8909b9299e2e3e
Caheri <caheri.aguilar@gmail.com>
2024-04-07 11:08:03 -0500

    can delete videos

--- src/client/estilocalico.js
+++ src/client/estilocalico.js
@@ -1,205 +1,206 @@
 async function allEvents(token) {
-    const response = await fetch(`${BACKEND_URL}/events`, {
-        method: 'GET',
-        headers: {
-            Authorization: token
-        }
-    });
-    const events = response.json();
-    return events;
+  const response = await fetch(`${BACKEND_URL}/events`, {
+    method: "GET",
+    headers: {
+      Authorization: token,
+    },
+  });
+  const events = response.json();
+  return events;
 }
 
 async function getEvent(eventId, token) {
-    const response = await fetch(`${BACKEND_URL}/events/${eventId}`, {
-        method: 'GET',
-        headers: {
-            Authorization: token
-        }
-    });
-    const event = response.json()
-    console.log(event)
-    return event
+  const response = await fetch(`${BACKEND_URL}/events/${eventId}`, {
+    method: "GET",
+    headers: {
+      Authorization: token,
+    },
+  });
+  const event = response.json();
+  console.log(event);
+  return event;
 }
 
 async function createEvent(eventObj, token) {
-    const response = await fetch(`${BACKEND_URL}/events`, {
-        method: 'POST',
-        headers: {
-            "Content-Type": "application/json",
-            Authorization: token
-        },
-        body: JSON.stringify(eventObj)
-    });
-    const newEvent = response.json();
-    return newEvent;
+  const response = await fetch(`${BACKEND_URL}/events`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: token,
+    },
+    body: JSON.stringify(eventObj),
+  });
+  const newEvent = response.json();
+  return newEvent;
 }
 
 async function deleteEvent(eventId, token) {
-    const response = await fetch(`${BACKEND_URL}/events/${eventId}`, {
-        method: 'DELETE',
-        headers: {
-            Authorization: token,
-        }
-    });
-    return "Event was deleted";
+  const response = await fetch(`${BACKEND_URL}/events/${eventId}`, {
+    method: "DELETE",
+    headers: {
+      Authorization: token,
+    },
+  });
+  return "Event was deleted";
 }
 
 async function updateEvent(eventObj, eventId, token) {
-    console.log("[updating event] idToken", token)
-    const response = await fetch(`${BACKEND_URL}/events/${eventId}`, {
-        method: 'PATCH',
-        headers: {
-            "Content-type": "application/json",
-            Authorization: token,
-        },
-        body: JSON.stringify(eventObj)
-    });
-    const updatedEvent = response.json()
-    return updatedEvent;
+  console.log("[updating event] idToken", token);
+  const response = await fetch(`${BACKEND_URL}/events/${eventId}`, {
+    method: "PATCH",
+    headers: {
+      "Content-type": "application/json",
+      Authorization: token,
+    },
+    body: JSON.stringify(eventObj),
+  });
+  const updatedEvent = response.json();
+  return updatedEvent;
 }
 
 // get code to exchange for token from query params
 async function getAuthToken(code) {
-    let url = "https://estilocalico.auth.us-east-2.amazoncognito.com/oauth2/token"
+  let url =
+    "https://estilocalico.auth.us-east-2.amazoncognito.com/oauth2/token";
 
-    let postOptions = {
-        'method': 'POST',
-        'headers': {
-            'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        'body': new URLSearchParams({
-            'grant_type': 'authorization_code',
-            'client_id': '7nk2p2fkrha638okq6m2tbobir',
-            'code': `${code}`,
-            'redirect_uri': FRONTEND_URL
-        })
-    }
-    console.log("[get auth token] request options", postOptions)
-    const authToken = await fetch(url, postOptions)
-        .then(response => response.json())
-        .then(tokenResponse => {
-            console.log('[get auth token] id_token response', tokenResponse.id_token)
-            return tokenResponse.id_token;
-        });
-    // console.log(authToken)
-    return authToken;
+  let postOptions = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: new URLSearchParams({
+      grant_type: "authorization_code",
+      client_id: "7nk2p2fkrha638okq6m2tbobir",
+      code: `${code}`,
+      redirect_uri: FRONTEND_URL,
+    }),
+  };
+  console.log("[get auth token] request options", postOptions);
+  const authToken = await fetch(url, postOptions)
+    .then((response) => response.json())
+    .then((tokenResponse) => {
+      console.log("[get auth token] id_token response", tokenResponse.id_token);
+      return tokenResponse.id_token;
+    });
+  // console.log(authToken)
+  return authToken;
 }
 
 async function allBios(token) {
-    const response = await fetch(`${BACKEND_URL}/bios`, {
-        method: 'GET',
-        headers: {
-            Authorization: token
-        }
-    });
-    const bios = response.json();
-    return bios;
+  const response = await fetch(`${BACKEND_URL}/bios`, {
+    method: "GET",
+    headers: {
+      Authorization: token,
+    },
+  });
+  const bios = response.json();
+  return bios;
 }
 
 async function getBio(bioId, token) {
-    const response = await fetch(`${BACKEND_URL}/bios/${bioId}`, {
-        method: 'GET',
-        headers: {
-            Authorization: token
-        }
-    });
-    const bio = response.json()
-    return bio
+  const response = await fetch(`${BACKEND_URL}/bios/${bioId}`, {
+    method: "GET",
+    headers: {
+      Authorization: token,
+    },
+  });
+  const bio = response.json();
+  return bio;
 }
 
 async function createBio(bioObj, token) {
-    const response = await fetch(`${BACKEND_URL}/bios`, {
-        method: 'POST',
-        headers: {
-            "Content-Type": "application/json",
-            Authorization: token
-        },
-        body: JSON.stringify(bioObj)
-    });
-    const newBio = response.json();
-    return newBio;
+  const response = await fetch(`${BACKEND_URL}/bios`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: token,
+    },
+    body: JSON.stringify(bioObj),
+  });
+  const newBio = response.json();
+  return newBio;
 }
 
 async function deleteBio(bioId, token) {
-    const response = await fetch(`${BACKEND_URL}/bios/${bioId}`, {
-        method: 'DELETE',
-        headers: {
-            Authorization: token,
-        }
-    });
-    return "Bio was deleted";
+  const response = await fetch(`${BACKEND_URL}/bios/${bioId}`, {
+    method: "DELETE",
+    headers: {
+      Authorization: token,
+    },
+  });
+  return "Bio was deleted";
 }
 
 async function updateBio(bioObj, token) {
-    // console.log("[updating event] idToken", token)
-    const response = await fetch(`${BACKEND_URL}/bios/${bioObj["id"]}`, {
-        method: 'PATCH',
-        headers: {
-            "Content-type": "application/json",
-            Authorization: token,
-        },
-        body: JSON.stringify(bioObj)
-    });
-    const updatedEvent = response.json()
-    return updatedEvent;
+  // console.log("[updating event] idToken", token)
+  const response = await fetch(`${BACKEND_URL}/bios/${bioObj["id"]}`, {
+    method: "PATCH",
+    headers: {
+      "Content-type": "application/json",
+      Authorization: token,
+    },
+    body: JSON.stringify(bioObj),
+  });
+  const updatedEvent = response.json();
+  return updatedEvent;
 }
 
 async function allVideos() {
-    const response = await fetch(`${BACKEND_URL}/videos`, {
-        method: 'GET'
-    })
-    const videos = response.json()
-    return videos;   
+  const response = await fetch(`${BACKEND_URL}/videos`, {
+    method: "GET",
+  });
+  const videos = response.json();
+  return videos;
 }
 
 async function createVideo(formData) {
-    const response = await fetch(`${BACKEND_URL}/videos`, {
-        method: 'POST',
-        body: formData
-    })
+  const response = await fetch(`${BACKEND_URL}/videos`, {
+    method: "POST",
+    body: formData,
+  });
 
-    const newVid = response.json()
-    return newVid;
+  const newVid = response.json();
+  return newVid;
 }
 
 async function updateMetadata(videoObj) {
-    // console.log("[updating event] idToken", token)
-    const response = await fetch(`${BACKEND_URL}/videos/${videoObj.id}`, {
-        method: 'PATCH',
-        headers: {
-            "Content-type": "application/json",
-            // Authorization: token,
-        },
-        body: JSON.stringify(videoObj)
-    });
-    const updatedEvent = response.json()
-    return updatedEvent;
+  // console.log("[updating event] idToken", token)
+  const response = await fetch(`${BACKEND_URL}/videos/${videoObj.id}`, {
+    method: "PATCH",
+    headers: {
+      "Content-type": "application/json",
+      // Authorization: token,
+    },
+    body: JSON.stringify(videoObj),
+  });
+  const updatedEvent = response.json();
+  return updatedEvent;
 }
 
 async function deleteVideo(videoId) {
-    const response = await fetch(`${BACKEND_URL}/videos/${videoId}`, {
-        method: 'DELETE',
-        headers: {
-            Authorization: token,
-        }
-    });
-    return "Video was deleted";
+  const response = await fetch(`${BACKEND_URL}/videos/${videoId}`, {
+    method: "DELETE",
+    // headers: {
+    //     Authorization: token,
+    // }
+  });
+  return "Video was deleted";
 }
 
-export { 
-    allEvents, 
-    getEvent, 
-    createEvent, 
-    deleteEvent, 
-    updateEvent, 
-    getAuthToken,
-    allBios,
-    getBio,
-    createBio,
-    updateBio,
-    deleteBio,
-    allVideos, 
-    createVideo,
-    updateMetadata,
-    deleteVideo
-    }
+export {
+  allEvents,
+  getEvent,
+  createEvent,
+  deleteEvent,
+  updateEvent,
+  getAuthToken,
+  allBios,
+  getBio,
+  createBio,
+  updateBio,
+  deleteBio,
+  allVideos,
+  createVideo,
+  updateMetadata,
+  deleteVideo,
+};
